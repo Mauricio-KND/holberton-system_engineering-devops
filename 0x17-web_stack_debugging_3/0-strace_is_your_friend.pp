14f65b6ee07ee35b552c464dc029904708877955
Mauricio-KND <1299@holbertonschool.com>
2020-05-19 23:21:32 +0000

    Updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# puppet script to correct a typo in a file
+# Correct typo.
 exec { 'modify_file':
   command => "/bin/sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php"
 }
